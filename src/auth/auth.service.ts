ab992c9183ff6c24713c9cd07751f3a5b83dfa5d
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-01-31 18:04:37 +0400

    Implement register method in Auth Service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { RegisterUserDTO } from './DTOs/register-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private _usersService: UsersService) {}
+
+  async register(registerUserDTO: RegisterUserDTO) {
+    const userExists = await this._usersService.findOneByEmail(
+      registerUserDTO.email,
+    );
+
+    if (!userExists) {
+      return this._usersService.create(registerUserDTO);
+    } else {
+      throw new BadRequestException(
+        'User with that email already exists. Please type new email!',
+      );
+    }
+  }
+}
