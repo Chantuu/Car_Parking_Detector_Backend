ba78ea25dba6e17efa4f9da8768f99e613087064
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-01-31 21:18:16 +0400

    Create messages.variables.ts
     Created this file to store all error message variables in one place for easy refactoring.

---
+++ src/helper/messages/messages.variables.ts
@@ -0,0 +1,11 @@
+export const emailAlreadyExistsErrorMessage =
+  'User with that email already exists. Please type new email!';
+
+export const userWithEmailPasswordNotExistsErrorMessage =
+  'User does not exist with that email or password.';
+
+export const unauthorizedErrorMessage =
+  'You are not authorized to view this content. Please register or log in!';
+
+export const userIdErrorMessage =
+  'No user was found with that id. Please input correct id!';
