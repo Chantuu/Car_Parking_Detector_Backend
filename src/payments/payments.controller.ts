f02c40116304ebf30f8f0ce77f9cc647dc5180d5
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 01:35:44 +0400

    Implement POST api/payments/paymentCard Endpoint

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AddPaymentCardDTO } from './DTOs/add-payment-card.dto';
+import { PaymentsService } from './payments.service';
+import { CurrentUser } from 'src/helper/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { AuthGuard } from 'src/auth/auth.guard';
 
-@Controller('payments')
-export class PaymentsController {}
+@Controller('api/payments')
+@UseGuards(AuthGuard)
+export class PaymentsController {
+  constructor(private _paymentsService: PaymentsService) {}
+
+  @Post('paymentCard')
+  async addPaymentCard(
+    @Body() addPaymentCardDto: AddPaymentCardDTO,
+    @CurrentUser() user: User,
+  ) {
+    return await this._paymentsService.addPaymentCard(addPaymentCardDto, user);
+  }
+}
