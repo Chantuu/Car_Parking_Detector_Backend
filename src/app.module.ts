fcc6c143e6766dd906f96de82bdb16676bd1bef3
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-01-31 18:56:25 +0400

    Import ConfigModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'main.db',
