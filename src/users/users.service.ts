34a3d560d66b2c8e353de8f9b25e9f505aeb623b
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-01-31 16:13:47 +0400

    Implement Users Service Functionality
      Implemented typorm repository management functionality inside this service.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private _userRepository: Repository<User>,
+  ) {}
+
+  findOne(userId: string) {
+    return this._userRepository.findOne({ where: { id: userId } });
+  }
+
+  create(user: User) {
+    const newUser = this._userRepository.create(user);
+    return this._userRepository.save(newUser);
+  }
+
+  async delete(userId: string) {
+    const foundUser = await this.findOne(userId);
+
+    if (foundUser) {
+      return this._userRepository.remove([foundUser]);
+    } else {
+      throw new Error(
+        'No user was found with that id. Please input correct id!',
+      );
+    }
+  }
+}
