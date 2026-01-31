e6c27cd33b287716ee6caf9b0b0a602e376d4885
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-01-31 18:25:01 +0400

    Implement login functionality in Auth Service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
+import { LoginUserDTO } from './DTOs/login-user.dto';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterUserDTO } from './DTOs/register-user.dto';
+import { comparePassword } from 'src/helper/functions/compare-password.function';
 
 @Injectable()
 export class AuthService {
@@ -16,6 +18,26 @@ export class AuthService {
     } else {
       throw new BadRequestException(
         'User with that email already exists. Please type new email!',
+      );
+    }
+  }
+
+  async login(loginUserDTO: LoginUserDTO) {
+    const userExists = await this._usersService.findOneByEmail(
+      loginUserDTO.email,
+    );
+
+    if (
+      userExists &&
+      (await comparePassword(loginUserDTO.password, userExists.passwordHashed))
+    ) {
+      return {
+        success: true,
+        message: `Successfully signed in user ${userExists.fullName}`,
+      };
+    } else {
+      throw new BadRequestException(
+        'User does not exist with that email or password.',
       );
     }
   }
