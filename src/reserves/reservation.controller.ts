20966ece20433d3e3ebef51edb2c41bd83d5913f
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 22:07:08 +0400

    Implement POST /api/reservation Endpoint

--- src/reserves/reservation.controller.ts
+++ src/reserves/reservation.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { CurrentUser } from 'src/helper/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { ReservationService } from './reservation.service';
+import { CreateReservationDTO } from './DTOs/create-reservation.dto';
 
-@Controller('reserves')
-export class ReservationController {}
+@Controller('api/reservation')
+@UseGuards(AuthGuard)
+export class ReservationController {
+  constructor(private _reservationService: ReservationService) {}
+
+  @Post()
+  async createReservation(
+    @CurrentUser() user: User,
+    @Body() createReservationDTO: CreateReservationDTO,
+  ) {
+    return await this._reservationService.createReservation(
+      user,
+      createReservationDTO,
+    );
+  }
+}
