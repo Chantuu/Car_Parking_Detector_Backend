f146305b8346b7fbbd31855d271047c0518e64bb
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 20:21:11 +0400

    Add Indexes in Reservation Entity
      Added new partial indexes in reservation entity, where it would allow only one active reservation per user and one user per parking spot.

--- src/reserves/reservation.entity.ts
+++ src/reserves/reservation.entity.ts
@@ -1,14 +1,30 @@
 import { ParkingSpot } from 'src/parking/parking-spot.entity';
 import { User } from 'src/users/user.entity';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  Index,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
+/// 1. Lock: One active reservation per User
+@Index('UQ_ACTIVE_USER', ['user'], { unique: true, where: 'isActive = 1' })
+// 2. Lock: One active user per Parking Spot
+@Index('UQ_ACTIVE_SPOT', ['parkingSpot'], {
+  unique: true,
+  where: 'isActive = 1',
+})
 export class Reservation {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({ type: 'datetime', nullable: false })
   startTime: Date;
+
+  @Column({ type: 'boolean', nullable: false, default: true })
+  isActive: boolean;
 
   @ManyToOne(() => User, (user) => user.reservations)
   user: User;
