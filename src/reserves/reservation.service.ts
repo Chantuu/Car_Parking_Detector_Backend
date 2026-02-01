7dca98273dd4f0f1e85a9e78ef09383c587ac6a4
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 22:06:55 +0400

    Implement Reservation Creation Functionality in ReservationService

--- src/reserves/reservation.service.ts
+++ src/reserves/reservation.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Reservation } from './reservation.entity';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import {
+  activeReservationAlreadyExitsErrorMessage,
+  parkingSpotIdErrorMessage,
+} from 'src/helper/messages/messages.variables';
+import { CreateReservationDTO } from './DTOs/create-reservation.dto';
+import { ParkingService } from 'src/parking/parking.service';
 
 @Injectable()
-export class ReservationService {}
+export class ReservationService {
+  constructor(
+    @InjectRepository(Reservation)
+    private _reservationRepository: Repository<Reservation>,
+    private _parkingService: ParkingService,
+  ) {}
+
+  async createReservation(
+    currentUser: User,
+    createReservationDTO: CreateReservationDTO,
+  ) {
+    const isActiveReservation = await this._reservationRepository.findOne({
+      where: { user: currentUser },
+    });
+    const currentParkingSpot = await this._parkingService.getParkingSpotById(
+      createReservationDTO.parkingSpotId,
+    );
+
+    if (!isActiveReservation && currentParkingSpot) {
+      const newReservation = this._reservationRepository.create({
+        user: currentUser,
+        parkingSpot: currentParkingSpot,
+        startTime: new Date(),
+      });
+
+      await this._reservationRepository.save(newReservation);
+
+      return {
+        status: 'success',
+        data: {
+          id: newReservation.id,
+          userId: currentUser.id,
+          parkingSpotId: currentParkingSpot.id,
+          startTime: newReservation.startTime,
+          status: newReservation.status,
+        },
+      };
+    } else if (!currentParkingSpot) {
+      throw new BadRequestException(parkingSpotIdErrorMessage);
+    } else {
+      throw new BadRequestException(activeReservationAlreadyExitsErrorMessage);
+    }
+  }
+}
