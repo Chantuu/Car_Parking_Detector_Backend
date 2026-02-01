c41bd1984f4a147a436fb66b70e88c3e94e979ee
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 11:56:41 +0400

    Implement GET api/parking/parkingSpots

--- src/parking/parking.controller.ts
+++ src/parking/parking.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { ParkingService } from './parking.service';
+import { parkingSpotsNotRegisteredErrorMessage } from 'src/helper/messages/messages.variables';
 
-@Controller('parking')
-export class ParkingController {}
+@Controller('api/parking')
+@UseGuards(AuthGuard)
+export class ParkingController {
+  constructor(private _parkingService: ParkingService) {}
+
+  @Get('parkingSpots')
+  async getAllParkingSpots() {
+    const parkingSpotArray = await this._parkingService.getAllParkingSpots();
+
+    if (parkingSpotArray.length > 0) {
+      return {
+        status: 'success',
+        data: parkingSpotArray,
+      };
+    } else {
+      throw new NotFoundException(parkingSpotsNotRegisteredErrorMessage);
+    }
+  }
+}
