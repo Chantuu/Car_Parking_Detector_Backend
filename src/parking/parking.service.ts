91046afdd9974e911a3a3999a0d6fd91c79a2083
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 11:56:08 +0400

    Create getAllParkingSpots method in ParkingService

--- src/parking/parking.service.ts
+++ src/parking/parking.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ParkingSpot } from './parking-spot.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ParkingService {}
+export class ParkingService {
+  constructor(
+    @InjectRepository(ParkingSpot)
+    private _parkingSpotRepository: Repository<ParkingSpot>,
+  ) {}
+
+  getAllParkingSpots() {
+    return this._parkingSpotRepository.find({});
+  }
+}
