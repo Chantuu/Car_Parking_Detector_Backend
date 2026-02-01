b1dbd8467c4ec1a0a5b54ee6743accb146841e20
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 11:31:49 +0400

    Create ParkingSpot Entity Class

---
+++ src/parking/parking-spot.entity.ts
@@ -0,0 +1,25 @@
+import { ParkingSpotStatus } from 'src/helper/enums/parking-spot-status.enum';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class ParkingSpot {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text', nullable: false })
+  spotName: string;
+
+  @Column({ type: 'enum', enum: ParkingSpotStatus, nullable: false })
+  status: string;
+
+  @Column({ type: 'text', nullable: false, unique: true })
+  sensorId: string;
+
+  @UpdateDateColumn()
+  lastUpdated: Date;
+}
