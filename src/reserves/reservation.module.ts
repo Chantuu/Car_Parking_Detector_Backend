d72da7fe8a6abd4f77391ac15c3cc0b5f6f2f8d9
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 20:22:08 +0400

    Import AuthModule inside ReservesModule

--- src/reserves/reservation.module.ts
+++ src/reserves/reservation.module.ts
@@ -3,9 +3,10 @@ import { ReservesController } from './reservation.controller';
 import { ReservesService } from './reservation.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reservation } from './reservation.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reservation])],
+  imports: [TypeOrmModule.forFeature([Reservation]), AuthModule],
   controllers: [ReservesController],
   providers: [ReservesService],
 })
