3de8aa64c6f8ee3d9c9366f85e32d9fd0057c4ff
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 18:01:02 +0400

    Implement proper saving functionality of new sensor data in ParkingService
     Added saveSpotDataFromSensor method, which automatically registers new spots sent from hardware sensor. If registered automatically updates them.

--- src/parking/parking.service.ts
+++ src/parking/parking.service.ts
@@ -1,7 +1,9 @@
+import { SensorDataDTO } from './Dtos/sensor-data.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ParkingSpot } from './parking-spot.entity';
 import { Repository } from 'typeorm';
+import { ParkingSpotStatus } from 'src/helper/enums/parking-spot-status.enum';
 
 @Injectable()
 export class ParkingService {
@@ -12,5 +14,44 @@ export class ParkingService {
 
   getAllParkingSpots() {
     return this._parkingSpotRepository.find({});
+  }
+
+  async saveSpotDataFromSensor(sensorDataDTO: SensorDataDTO) {
+    const existingSpots = await this.getAllParkingSpots();
+
+    const existingSpotsMap = new Map(
+      existingSpots.map((spot) => [spot.sensorId, spot]),
+    );
+
+    const spotsToPersist = sensorDataDTO.parkingSpots.map((incomingSpotDTO) => {
+      const persistedSpot = existingSpotsMap.get(incomingSpotDTO.sensorId);
+
+      if (persistedSpot) {
+        const isLocked =
+          persistedSpot.status === ParkingSpotStatus.RESERVED ||
+          persistedSpot.status === ParkingSpotStatus.RESERVED_CHECK;
+
+        return {
+          ...persistedSpot,
+          ...incomingSpotDTO,
+          status: isLocked ? persistedSpot.status : incomingSpotDTO.status,
+        };
+      }
+
+      return incomingSpotDTO;
+    });
+
+    const formattedResponseData = (
+      await this._parkingSpotRepository.save(spotsToPersist)
+    ).map((updatedSpot) => ({
+      spotName: updatedSpot.spotName,
+      status: updatedSpot.status,
+      sensorId: updatedSpot.sensorId,
+    }));
+
+    return {
+      status: 'success',
+      updatedParkingSpots: formattedResponseData,
+    };
   }
 }
